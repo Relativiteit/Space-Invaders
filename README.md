da25c58492578c29449a6bc5ed668e40fd7e7ed1
Relativiteit <alejocain@gmail.com>
2021-06-09 16:24:52 +0200

    Changed structure to OOP

--- README.md
+++ README.md
@@ -8,14 +8,20 @@ Hopefully staying true to KISS it will be possible to make a fun tool for me and
 ## 1. Space-Invaders - Done 14-05-2021
 A 2 player shooter where the objective is to shoot down eachother's spaceship.
 
-##### Lesson learned 
+##### Lessons learned 
 How to map keys to actions, how to render objects, how to setup events, how to register multiple keystrokes at the same time.
 
 ##### What can be improved?
 The file is messy with allot of constants on the top of the file. Next small game use OOP to structure the functions in a class. 
 
-## 2. Snake 
+## 2. Snake - Done 09-06-2021
 A classic game where the objective is to get as much tokens in the screens as possible, growing your players body. While not touching your own body.
+#### Lessons learned?
+How to draw surfaces with .blit(x,y) to move images to different positions.
+
+#### What can be improved?
+Taking into account time complexity, and space of code.
+
 
 https://www.youtube.com/watch?v=8dfePlONtls&t=85s
 
@@ -23,21 +29,24 @@ https://www.youtube.com/watch?v=8dfePlONtls&t=85s
 # Dnd roll 20 alternative 
 #### 1. Range based system *grid - Final Fnatasy tactics - HEXAGON www.redblobgames.com/grids/hexagons.
 
-#### 2. Characters objects met states
+#### 2. Characters objects with states
+- health 
+- poisoned, paralyzed, frightend, dehydrated 
+- advantage attack because of position 
 
 #### 3. Webscraping char sheets exp
-    get request website (exp > ... get http/ REST )
+- get request website (exp > ... get http/ REST )
 
 #### 4. Dashboard GM
     - health, * mobs 
     - Mob creator: name attack hitpoints > avatar 
-    Randomzier button of mobs 
+    - Randomzier button of mobs 
 
 #### 5. Sounds Design 
-    - hit collusion 
+    - hit collision 
     - Battle music 
     - LG Soundboard guy 
-Automate with spotify 
+    - Automate with spotify 
 
 #### Isometric rendering 2d (3d)
 
@@ -46,5 +55,5 @@ https://www.youtube.com/watch?v=gE2gTCwLdFM
 ## Extra shenanigans
 Spotify ETL, just playing around with ETL (extration, transform, load).
 Grabbing most played songs from the past 60 days, storing them in sqlite database.
-Using DBeaver to query data base 
+Using DBeaver to query data base.
 
--- Snake/main.py
+++ Snake/main.py
@@ -1,58 +1,69 @@
+# Make it all object oriented by converting the code in various classes
+
 import pygame
 from pygame.locals import *
 
 class Snake:
-    def __init__(self, parent_screen):
-        self.parent_screen = parent_screen
-        self.block = pygame.image.load("resources/block.jpg").convert()
-        self.block_x = 100
-        self.block_y = 100
-        pass
+    def __init__(self, surface):
+        self.parent_screen = surface
+        self.block = pygame.image.load("Space-Invaders/Snake/block.jpg").convert()
+        self.x = 100
+        self.y = 100
 
-    def draw(self,surface):
-        surface.fill((60, 209, 197))
-        surface.blit(block, (block_x, block_y))
+    def move_left(self):
+        self.x -= 10
+        self.draw()
+
+    def move_right(self):
+        self.x += 10
+        self.draw()
+
+    def move_up(self):
+        self.y -= 10
+        self.draw()
+
+    def move_down(self):
+        self.y += 10
+        self.draw()
+
+    def draw(self):
+        self.parent_screen.fill((110, 110, 5))
+
+        self.parent_screen.blit(self.block, (self.x, self.y))
         pygame.display.flip()
-        surface.parent_screen.blit(self.block,(self.block_x, self.block_y)) 
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((1000, 500))
-        self.surface.fill((60, 209, 197))
+        self.surface = pygame.display.set_mode((500, 500))
         self.snake = Snake(self.surface)
         self.snake.draw()
 
-        pass
-
     def run(self):
-        
-        pass
+        running = True
 
-def draw_block():
-    surface.fill((60, 209, 197))
-    surface.blit(block, (block_x, block_y))
-    pygame.display.flip()
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
 
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
 
-if __name__ == "__main__":
+                    if event.key == K_UP:
+                        self.snake.move_up()
+
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+
+                elif event.type == QUIT:
+                    running = False
+
+if __name__ == '__main__':
     game = Game()
     game.run()
-
-   
-   pygame.display.flip()
-    
-    running = True
-    
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-                if event.key == K_UP:
-                    pass
-
-            elif event.type == QUIT:
-                running = False
 
